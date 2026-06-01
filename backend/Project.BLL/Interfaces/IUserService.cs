8106e9e291b96d4c77e817fb6fca77e1e945e652
Amr <am2873328@gmail.com>
2026-06-01 20:08:50 +0200

    Add Maping + service

--- backend/Project.BLL/Interfaces/IUserService.cs
+++ backend/Project.BLL/Interfaces/IUserService.cs
@@ -1,12 +1,13 @@
+using Common.Results;
 using Project.BLL.DTOs;
 
 namespace Project.BLL.Interfaces;
 
 public interface IUserService
 {
-    Task<IEnumerable<UserDto>> GetAllAsync();
-    Task<UserDto> GetByIdAsync(int id);
-    Task<UserDto> CreateAsync(CreateUserDto dto);
-    Task<UserDto> UpdateAsync(int id, UpdateUserDto dto);
-    Task DeleteAsync(int id);
+    Task<OperationResult<IEnumerable<UserDto>>> GetAllAsync();
+    Task<OperationResult<UserDto>> GetByIdAsync(int id);
+    Task<OperationResult<UserDto>> CreateAsync(CreateUserDto dto);
+    Task<OperationResult<UserDto>> UpdateAsync(int id, UpdateUserDto dto);
+    Task<OperationResult> DeleteAsync(int id);
 }
